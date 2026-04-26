50cd68a96fd282b59f4096cd6ea9087e496d0e5a
Natan-Barbosa <natanbarbosax193@gmail.com>
2026-04-26 19:28:10 -0300

    feat: add validation in dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsStrongPassword, Length } from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
+export class CreateUserDto {
+    @IsEmail()
+    @IsNotEmpty({ message: 'Email is required' })
+    email!: string;
+
+    @IsNotEmpty({ message: 'Password is required' })
+    @IsStrongPassword()
+    @Length(8, 20, { message: 'Password must be between 8 and 20 characters' })
+    password!: string;
+
+    @IsOptional()
+    @IsEnum(UserRole, { message: 'Role must be either ADMIN or USER' })
+    userRole?: UserRole;
+}
