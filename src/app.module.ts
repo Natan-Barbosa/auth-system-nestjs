0de7e3c1850568b2b4cc13a71ebdf1a75d11aad7
Natan-Barbosa <natanbarbosax193@gmail.com>
2026-04-26 19:27:16 -0300

    feat: add new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './infra/database/database.module';
+import { UserRepositoryModule } from './infra/repositories/user.repository.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, DatabaseModule, UserRepositoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
