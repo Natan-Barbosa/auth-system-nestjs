f5328ef6d60bf7aeec5c2d838dc1e5cfa9b978eb
Natan-Barbosa <natanbarbosax193@gmail.com>
2026-04-26 19:28:24 -0300

    feat: add user entity definition

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,28 @@
-export class User {}
+import { Column, Entity } from "typeorm";
+
+export enum UserRole {
+    ADMIN = 'ADMIN',
+    USER = 'USER',
+}
+
+@Entity('users')
+export class User {
+
+    @Column({ type: 'uuid', primary: true, default: () => 'UUID()' })
+    id!: string;
+
+    @Column({ type: 'varchar', length: 255, unique: true })
+    email!: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    password!: string;
+
+    @Column({ type: 'enum', enum: UserRole, default: UserRole.USER })
+    userRole!: UserRole;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    createdAt!: Date;
+
+    @Column({ type: 'boolean', default: true })
+    isActive!: boolean;
+}
