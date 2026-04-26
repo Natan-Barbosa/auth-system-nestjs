e6b6112c9ba66c1fa536b3992d407b62d015d9b6
Natan-Barbosa <natanbarbosax193@gmail.com>
2026-04-26 19:58:40 -0300

    feat: throws exception when user not found

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UserRepository } from 'src/infra/repositories/user.repository';
+import { BcryptService } from 'src/shared/bcrypt.service';
 
 @Injectable()
 export class UserService {
 
-  constructor(private readonly userRepository: UserRepository) { }
+  constructor(private readonly userRepository: UserRepository, private readonly bcryptService: BcryptService) { }
 
   async create(createUserDto: CreateUserDto) {
-    await this.userRepository.create(createUserDto);
+    const { password } = createUserDto;
+    const hashedPassword = await this.bcryptService.encryptPassword(password);
+    const userDtoWithHashedPassword = { ...createUserDto, password: hashedPassword };
+    await this.userRepository.create(userDtoWithHashedPassword);
   }
 
   async findAll() {
@@ -17,10 +21,22 @@ export class UserService {
   }
 
   async findOne(id: string) {
-    return await this.userRepository.findOne(id);
+    const user = await this.userRepository.findOne(id);
+    if (!user) {
+      throw new NotFoundException(`User with id: ${id} not found`);
+    }
+    return user;
   }
 
   async update(id: string, updateUserDto: UpdateUserDto) {
+    if (!await this.userRepository.existsById(id)) {
+      throw new NotFoundException(`User with id: ${id} not found`);
+    }
+    const { password } = updateUserDto;
+    if (password) {
+      const hashedPassword = await this.bcryptService.encryptPassword(password);
+      updateUserDto.password = hashedPassword;
+    }
     return await this.userRepository.update(id, updateUserDto);
   }
 
