ff157c46b3750bb9ef345a5dd62998bc9265cffc
Natan-Barbosa <natanbarbosax193@gmail.com>
2026-04-26 19:58:50 -0300

    feat: add bcrypt service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepositoryModule } from 'src/infra/repositories/user.repository.module';
+import { BcryptService } from 'src/shared/bcrypt.service';
 
 @Module({
   imports: [UserRepositoryModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, BcryptService],
 })
 export class UserModule { }
